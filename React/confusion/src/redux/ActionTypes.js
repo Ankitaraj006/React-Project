e9470140017001f44865cf354e0db96fb2547997
Ankita Raj <ankita.raj006@gmail.com>
2022-01-30 20:35:26 +0530

    first commit

---
+++ React/confusion/src/redux/ActionTypes.js
@@ -0,0 +1,25 @@
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const ADD_FEEDBACK = 'ADD_FEEDBACK';
+
+/*----------For Dishes---------*/
+
+export const DISHES_LOADING = 'DISHES_LOADING';
+export const DISHES_FAILED = 'DISHES_FAILED';
+export const ADD_DISHES = 'ADD_DISHES';
+
+/*----------For Comments---------*/
+
+export const ADD_COMMENTS = 'ADD_COMMENTS';
+export const COMMENTS_FAILED = 'COMMENTS_FAILED';
+
+/*----------For Promos---------*/
+
+export const PROMOS_LOADING = 'PROMOS_LOADING';
+export const ADD_PROMOS = 'ADD_PROMOS';
+export const PROMOS_FAILED = 'PROMOS_FAILED';
+
+/*----------For Leaders---------*/
+
+export const LEADERS_LOADING ='LEADERS_LOADING';
+export const ADD_LEADERS = 'ADD_LEADERS';
+export const LEADERS_FAILED = 'LEADERS_FAILED';
